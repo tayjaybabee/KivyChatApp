93fa3cc5ffb07bf834e7b9ab1720667b8c14c63d
Taylor B. <tayjaybabee@gmail.com>
2024-10-19 17:06:57 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+Author: Taylor B. tayjaybabee@gmail.com
+Date: 2024-10-19 17:02:07
+LastEditors: Taylor B. tayjaybabee@gmail.com
+LastEditTime: 2024-10-19 17:04:27
+FilePath: main.py
+Description: 这是默认设置,可以在设置》工具》File Description中进行配置
+"""
+"""
+
+
+Author: 
+    Inspyre Softworks
+
+Project:
+    KivyChatApp
+
+File: 
+    /main.py
+ 
+
+Description:
+    
+
+"""
